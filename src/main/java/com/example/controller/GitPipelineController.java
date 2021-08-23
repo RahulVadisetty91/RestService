af787c87c4b21e1e487b37d1c2e0a9d6f482b4a6
rahulvadisetty91 <smart.rahul456@gmail.com>
2021-08-23 22:08:12 +0530

    Pushing source code

---
+++ src/main/java/com/example/controller/GitPipelineController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitPipelineController {
+
+	private static final String template = "Hello, %s!";
+	
+	@GetMapping("/gitPipeline")
+	public String healthCheck() {
+		return String.format(template, "Git Pipeline!!");
+	}
+}
