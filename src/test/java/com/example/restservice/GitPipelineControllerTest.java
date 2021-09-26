e76b9af08314efe0efe67a442cb2a7ffa405e93e
rv <smart.rahul456@gmail.com>
2021-09-26 22:18:31 +0530

    Update GitPipelineControllerTest.java

--- src/test/java/com/example/restservice/GitPipelineControllerTest.java
+++ src/test/java/com/example/restservice/GitPipelineControllerTest.java
@@ -1,28 +1,22 @@
-package com.example.restservice;
+import static org.junit.Assert.assertNotNull;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.example.controller.GitPipelineController;
+import com.example.RestServiceApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class GitPipelineControllerTest {
+@SpringBootTest(classes = RestServiceApplication.class)
+public class SmokeHelloControllerTest {
 
 	@Autowired
-	private WebApplicationContext webApplicationContext;
-
-	private MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	}
+	private GitPipelineController gitPipelineController;
 
 	@Test
-	public void testEmployee() throws Exception {
-		mockMvc.perform(get("/getPipeline")).andExpect(status().isOk())
-				.andExpect(content().contentType("application/json;charset=UTF-8"));
-
+	public void testApplicationLload() {
+		assertNotNull("GitPipelineController is not loaded", gitPipelineController);
 	}
 }
