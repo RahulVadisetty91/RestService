9fb2099c38f78ac684613a489aeb5a20c44b8459
rv <smart.rahul456@gmail.com>
2021-09-26 22:29:40 +0530

    Update RestServiceApplicationTests.java

--- src/test/java/com/example/restservice/RestServiceApplicationTests.java
+++ src/test/java/com/example/restservice/RestServiceApplicationTests.java
@@ -3,12 +3,18 @@ package com.example.restservice;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.controller.GitPipelineController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 public class RestServiceApplicationTests {
 
+	@Autowired
+	GitPipelineController controller;
+	
 	@Test
 	public void contextLoads() {
+		System.out.println(controller.healthCheck());
 	}
 
 }
