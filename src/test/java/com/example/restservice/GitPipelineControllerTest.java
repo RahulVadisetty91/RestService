b7c29bf53749972ad5fb4e947aa061fb8762b319
rv <smart.rahul456@gmail.com>
2021-09-26 22:24:04 +0530

    Update GitPipelineControllerTest.java

--- src/test/java/com/example/restservice/GitPipelineControllerTest.java
+++ src/test/java/com/example/restservice/GitPipelineControllerTest.java
@@ -1,22 +1,42 @@
-import static org.junit.Assert.assertNotNull;
+package com.example.restservice;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.RunWith;
+import com.example.RestServiceApplication;
+import java.io.IOException;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import com.example.controller.GitPipelineController;
-import com.example.RestServiceApplication;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.junit.Test;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = RestServiceApplication.class)
-public class SmokeHelloControllerTest {
+@WebAppConfiguration
+public class GitPipelineControllerTest {
+	protected MockMvc mvc;
+   @Autowired
+   WebApplicationContext webApplicationContext;
 
-	@Autowired
-	private GitPipelineController gitPipelineController;
-
+   protected void setUp() {
+      mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+   }
 	@Test
-	public void testApplicationLload() {
-		assertNotNull("GitPipelineController is not loaded", gitPipelineController);
-	}
+public void getProductsList() throws Exception {
+   String uri = "/getPipeline";
+   MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+      .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+   
+   int status = mvcResult.getResponse().getStatus();
+   assertEquals(200, status);
+   String content = mvcResult.getResponse().getContentAsString();
+   Product[] productlist = super.mapFromJson(content, Product[].class);
+   assertTrue(productlist.length > 0);
+}
 }
